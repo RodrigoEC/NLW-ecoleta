156fb93271f02b7077c07cefd0b7464a0ebd963d
Rodrigo Eloy <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-06-03 21:46:02 -0300

    Metade da aula 03 e fim do formulario

--- script/create-point.js
+++ script/create-point.js
@@ -1,45 +1,50 @@
 
 
-function fecthLocalities(url, selector_name) {
+function fecthLocalities(url, selector, isCity) {
     fetch(url)
     .then(res =>  res.json() )
     .then(localities => {
         
-        selector = document.querySelector(selector_name)
-        console.log(selector.document + "vacas")
+        selector.innerHTML = `<option value="">selecionar localidade</option>`
 
-        // if (selector_name == "select[name=uf]") {
-        //     selector.innerHTML = `<option value="">selecionar o estado</option>`
-        // } else if (selector_name == "select[name=city]") {
-        //     selector.innerHTML = `<option value="">selecionar a cidade</option>`
-        // }   
-        selector.innerHTML = `<option value="">selecionar o local</option>`                                            
-        
+
+        let valueLocality
 
         for (var locality of localities) {
-            selector.innerHTML += `<option value="${locality.id}">${locality.nome}</option>`
+            if (isCity) {
+                valueLocality = locality.nome
+            } else {
+                valueLocality = locality.id
+            }
+
+            selector.innerHTML += `<option value="${valueLocality}">${locality.nome}</option>`
 
         }
     })
 }
 
 function populateUFs() {
-    fecthLocalities("https://servicodados.ibge.gov.br/api/v1/localidades/estados", "select[name=uf]")
+    const ufSelect = document.querySelector("select[name=uf]")
+
+
+    fecthLocalities("https://servicodados.ibge.gov.br/api/v1/localidades/estados",
+    ufSelect,
+    false
+    )
 }
 
 function getCities(event) {
     const citySelect = document.querySelector("select[name=city]")
     const stateInput = document.querySelector("input[name=state]")
 
-    const ufValue = event.target.value
 
     const indexSelectState = event.target.selectedIndex
     stateInput.value = event.target.options[indexSelectState].text
 
+    const ufValue = event.target.value
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
-
-    fecthLocalities(url, "select[name=city]")
+    fecthLocalities(url, citySelect, true)
 
     citySelect.disabled = false
 }
@@ -49,3 +54,104 @@ populateUFs()
 document
     .querySelector("select[name=uf]")
     .addEventListener('change', getCities)
+
+
+const collectedItems = document.querySelector("input[name=items")
+
+// Itens de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+
+for (item of itemsToCollect) {
+    item.addEventListener('click', handleSelectedItem)
+}
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    // add or remove a class with JS using the classlist method "toggle"
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex(
+        item => item == itemId 
+        // retorns true or false if the item was selected or not
+    )
+
+
+    if (alreadySelected != -1) {
+        // Remove from selected collection
+
+        const filteredItems = selectedItems.filter(item => item != itemId)
+
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+
+
+    collectedItems.value = selectedItems
+    console.log(collectedItems)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (item of itemsToCollect) {
+//     item.addEventListener('mouseover', addSelectedItem)
+//     item.addEventListener('mouseout', removeSelectedItem)
+// }
+
+// function addSelectedItem(event) {
+//     const itemLi = event.target
+
+//     // add or remove a class with JS using the classlist method "toggle"
+//     itemLi.classList.add("selected")
+
+//     const itemId = itemLi.dataset.id
+//     console.log()
+// }
+
+// function removeSelectedItem(event) {
+//     const itemLi = event.target
+
+//     itemLi.classList.remove("selected")
+
+//     const itemId = itemLi.dataset.id
+
+// }
