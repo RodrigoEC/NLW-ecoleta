8a640dd408e1e67548cae0c6ecb205b02315e7f1
Rodrigo Eloy <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-06-06 18:12:19 -0300

    Aula 04

--- src/server.js
+++ src/server.js
@@ -8,17 +8,25 @@ server.listen(3000)
 // configurar pasta pública
 server.use(express.static("public"))
 
+// Utilizando template engine
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    nocache: true 
+})
+
 // configurar caminhos na minha aplicação
 // página inicial
 
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
-server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
